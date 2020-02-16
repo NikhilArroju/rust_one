16809d4c9f67b0b2887acb440c4cf7c685f108cb
Nikhil Arroju <nikhilarroju47@gmail.com>
2020-02-16 15:40:33 +0530

    final cli

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 //mod print;
 //mod vars;
-mod types;
-
+//mod types;
+//mod strings;
+//mod tuples;
+//mod arrays;
+//mod vectors;
+//mod conditionals;
+//mod functions;
+//mod loops;
+//mod pointer_ref;
+mod cli;
+//mod enums;
+//mod structs;
 fn main() {
-    types::run();
+    cli::run();
 }
