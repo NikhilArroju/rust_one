4e50d231f51b33a3b808bc2b78db1c9ef15eef67
Nikhil Arroju <nikhilarroju47@gmail.com>
2020-02-16 14:21:54 +0530

    vectors

---
+++ src/vectors.rs
@@ -0,0 +1,22 @@
+//fixed length of same datatype
+pub fn run() {
+    let mut numbers: Vec<i32> = vec![1, 2, 3, 4, 50];
+    println!("{:?}", numbers);
+    println!("{}", numbers[4]);
+
+    numbers[0] = 1213;
+    println!("{:?}", &numbers[0..2]); //should use & find out why
+
+    //arrays are stack allocated
+    println!("{} bytes", std::mem::size_of_val(&numbers)); //we need to pass a reference &
+
+    numbers.push(5);
+    numbers.push(8);
+    numbers.pop();
+
+    println!("{:?}", numbers);
+
+    //slices
+    let slice: &[i32] = &numbers[0..3];
+    println!("{:?}", slice);
+}
